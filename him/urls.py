8591dba242e841ce2db87b639f90a93a178a4db2
wfcadd <wfcadd@gmail.com>
2018-06-08 17:56:40 +0800

    增加了header.html,footer.html以及一些static文件

--- him/urls.py
+++ him/urls.py
@@ -4,8 +4,10 @@
 from django.urls import path,re_path
 
 from . import views
-
+app_name = 'him'
 urlpatterns=[
     re_path('^$', views.blog_title,name='blog_title'),
     re_path('(?P<article_id>\d)/$',views.blog_article,name='blog_detail')
 ]
+
+
--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR+'/Templates'],
+        'DIRS': [os.path.join(BASE_DIR,'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,"static"),
+)
