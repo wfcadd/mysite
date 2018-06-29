33bdf08ceca7b5c964a7961da7743548c076b6f8
wfcadd <wfcadd@gmail.com>
2018-06-29 10:08:52 +0800

    增加了用户注册功能，及登录几面对注册的优化

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,14 @@ from django.urls import path,re_path
 from . import views
 
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from django.conf import settings
 app_name = 'account'
 urlpatterns = [
-    re_path('^login/$',views.user_login,name='user_login'),
+    #re_path('^login/$',views.user_login,name='user_login'),
+    #re_path('^login/$',auth_views.login,name='user_login'),
+    re_path('^new-login/$',auth_views.login,{"template_name":"account/login.html"},name='user_login'),
+    re_path('^logout/$',auth_views.logout,{"template_name":"account/logged_out.html"},name='user_logout'),
+    re_path('^register/$',views.register,name='user_register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
 
+
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm,UserProfileForm
+
+
+
+
 
 # Create your views here.
 
@@ -25,5 +30,25 @@ def user_login(request):
     if request.method == "GET":
         login_form = LoginForm()
         return render(request,"account/login.html",{"form":login_form})
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid() & userprofile_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponse("register successfully ")
+        else:
+            return HttpResponse("sorry,you cant register")
+    else:
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        return render(request,"account/register.html",{"form":user_form,"profile":userprofile_form})
 
 
